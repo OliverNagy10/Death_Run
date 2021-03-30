2e7797a36f2664beb527567a5c0016b9ef9bea7a
OliverNagy10 <81628953+OliverNagy10@users.noreply.github.com>
2021-03-30 15:55:56 +0100

    Add files via upload

---
+++ source/main.lua
@@ -0,0 +1,7 @@
+-----------------All  graphics were made by the team using pixel art maker ---------------------------------
+display.setStatusBar( display.HiddenStatusBar )
+local composer = require("composer")
+
+composer.gotoScene("mainmenu" , { effect = "fade"  , time = 500 } )
+
+
