08b371bebee4b3328b456823561729d76590db00
OliverNagy10 <81628953+OliverNagy10@users.noreply.github.com>
2021-04-12 18:42:43 +0100

    Delete source directory

--- source/main.lua
+++
@@ -1,7 +0,0 @@
------------------All  graphics were made by the team using pixel art maker ---------------------------------
-display.setStatusBar( display.HiddenStatusBar )
-local composer = require("composer")
-
-composer.gotoScene("mainmenu" , { effect = "fade"  , time = 500 } )
-
-
